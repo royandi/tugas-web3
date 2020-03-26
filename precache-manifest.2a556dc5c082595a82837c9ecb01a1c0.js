5051da1e3f79c31fce9e137666c454957ab312e0
gezaramdaciptaan <gezaramdaciptaan@gmail.com>
2020-03-26 20:21:07 +0700

    Updates

---
+++ precache-manifest.2a556dc5c082595a82837c9ecb01a1c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73da0438a045ed7434ede2de7886d42d",
+    "url": "/tugas-web3/index.html"
+  },
+  {
+    "revision": "b64dceca55853f6b49c5",
+    "url": "/tugas-web3/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "1168662fff62fbdbb9bf",
+    "url": "/tugas-web3/static/css/main.b1d03e56.chunk.css"
+  },
+  {
+    "revision": "b64dceca55853f6b49c5",
+    "url": "/tugas-web3/static/js/2.b8253ffb.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/tugas-web3/static/js/2.b8253ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1168662fff62fbdbb9bf",
+    "url": "/tugas-web3/static/js/main.1acc608c.chunk.js"
+  },
+  {
+    "revision": "dc403bbe2a90d583b770",
+    "url": "/tugas-web3/static/js/runtime-main.c3da19ce.js"
+  },
+  {
+    "revision": "1ab55ced3811392ae7b54c9a3d1bf428",
+    "url": "/tugas-web3/static/media/roy3.1ab55ced.jpg"
+  }
+]);
